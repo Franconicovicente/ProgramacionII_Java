0b899779a9126f453adc1e8e75f016235c59bf2f
Franconicovicente <franconicolassvicente@gmail.com>
2025-04-12 12:16:27 -0300

    Subiendo SRC

---
+++ clases/Clase2.java
@@ -0,0 +1,43 @@
+//package Clases;
+//
+//public class Clase2 {
+//
+//    public static void main(String[] args) {
+//  
+//        int x = 100; // int x;
+//                     // x = 5;
+//        int t = 200;            
+//       double s = 3.14;
+//       
+//       char z = 'f';
+//       
+//       // Wrapper
+//       
+//       String d = "Juan";
+//       
+//       Double q = 3.14;
+//       
+//       Integer y = 12;
+//       
+//       //
+//       
+//       int edad = 18;
+//       
+//       if (edad >= 18){
+//           System.out.println("Es mayor de edad");
+//       }
+//       
+//       else{
+//           System.out.println("No es mayor de edad");
+//       }
+//       
+//       
+//    //System.out.println(x);
+//        
+//    //System.out.println(y);
+//        
+//        
+//    }
+//    
+//}
+//
